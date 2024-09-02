363e4235cc9f6885146ec042021977cdd91abe36
endomorphosis <starworks5@gmail.com>
2024-09-01 20:54:03 -0700

    chore: Update package version to 1.0.4 and refactor export statement in index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ export * from './ipfs_accelerate_js/ipfs_accelerate.js';
 export { ipfsAccelerateJs } from './ipfs_accelerate_js/ipfs_accelerate.js';
 export { ipfsAccelerateJs as default } from './ipfs_accelerate_js/ipfs_accelerate.js';
 export { test_ipfs_accelerate } from './test/test.js';
-const testipfsAccelerateJs = new test_ipfs_accelerate();
-export { testipfsAccelerateJs };
+const test = new test_ipfs_accelerate();
+export { test } ;
