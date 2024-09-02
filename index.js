96b5604bb26c67a7986c683a65db28d9242113db
endomorphosis <starworks5@gmail.com>
2024-09-01 19:12:12 -0700

    chore: Update package version to 1.0.3 and dependencies in package.json

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 export * from './ipfs_accelerate_js/ipfs_accelerate.js';
 export { ipfsAccelerateJs } from './ipfs_accelerate_js/ipfs_accelerate.js';
 export { ipfsAccelerateJs as default } from './ipfs_accelerate_js/ipfs_accelerate.js';
+export { test_ipfs_accelerate } from './test/test.js';
+const test = new test_ipfs_accelerate();
+export { test };
