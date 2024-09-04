91cc24329d3ac260473ce47f637dfb22a6dd1f3c
endomorphosis <starworks5@gmail.com>
2024-09-04 01:05:08 -0700

    chore: Update package version to 1.0.7 and add new export in index.js

--- ipfs_accelerate_js/index.js
+++ ipfs_accelerate_js/index.js
@@ -1,2 +1,4 @@
 export { ipfsAccelerateJs } from './ipfs_accelerate.js';
 export { ipfsAccelerateJs as default } from './ipfs_accelerate.js';
+const ipfsAccelerate = new ipfsAccelerateJs();
+export { ipfsAccelerate } ;
